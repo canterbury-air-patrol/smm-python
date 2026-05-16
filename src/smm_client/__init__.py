419c26e441b0e738c1d1223e89a5c603f7bd80db
Scott Parlane <scott@parlanenz.com>
2026-05-16 21:45:26 +1200

    feat: export main classes in __init__.py

--- src/smm_client/__init__.py
+++ src/smm_client/__init__.py
@@ -1,3 +1,11 @@
 # SPDX-FileCopyrightText: 2024-present Canterbury Air Patrol Inc. <github@canterburyairpatrol.org>
 #
 # SPDX-License-Identifier: MIT
+"""
+Search Management Map (SMM) Client Library
+"""
+
+from smm_client.connection import SMMConnection
+from smm_client.types import SMMPoint
+
+__all__ = ["SMMConnection", "SMMPoint"]
